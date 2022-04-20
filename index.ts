38abee866951371b28f2206a447888959dd29d28
Phil <pbrede21@gmail.com>
2022-04-20 13:45:19 -0600

    slightly more memory efficient

--- index.ts
+++ index.ts
@@ -1,26 +1,26 @@
 import dotenv from "dotenv";
 import twilio from "twilio";
-import { ChannelInstance } from "twilio/lib/rest/chat/v2/service/channel";
 
 dotenv.config();
 
 const { ACCOUNT_SID, AUTH_TOKEN } = process.env;
 const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
-(async () => {
-  const publicChannels = await client.chat.v2
-    .services("ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    .channels.list({ type: "public" });
+async function updateChannel(channelSid: string) {
+  // update your channel to private here
+}
 
-  for (const channel of publicChannels) {
-    updateChannel(channel);
+(async () => {
+  const publicChannelSids = await client.chat.v2
+    .services("ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    .channels.list({ type: "public" })
+    .then((channels) => channels.map((channel) => channel.sid));
+
+  for (const channelSid of publicChannelSids) {
+    updateChannel(channelSid);
     await wait();
   }
 })();
-
-async function updateChannel(channel: ChannelInstance) {
-  // update your channel to private here
-}
 
 async function wait(ms = 1000) {
   return new Promise((resolve) => setTimeout(() => resolve(null), ms));
